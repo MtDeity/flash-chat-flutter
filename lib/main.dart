06ad6e0b72f91451608408fc84adffd763b8a025
MtDeity <o8olo853q43@gmail.com>
2020-03-14 20:34:47 +0900

    add kTextFieldDecoration constant

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class FlashChat extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           body1: TextStyle(color: Colors.black54),
+          subhead: TextStyle(color: Colors.black54),
         ),
       ),
       initialRoute: WelcomeScreen.id,
